d984fc7dd94bd6da4d2d3602286f0a737648c125
Vikas Kumar <vickynehra2016@gmail.com>
2025-06-01 19:16:10 +0530

    Added Student.java and updated Main.java

--- Main.java
+++ Main.java
@@ -8,5 +8,7 @@ public class Main{
         System.out.println(new Date());
         System.out.println("Added one more changes");
         System.out.println("More changes to check");
+
+        new Student().sayHello();
     }
 }
---
+++ Student.java
@@ -0,0 +1,10 @@
+public class Student {
+    
+    public Student(){
+        System.out.println("Creating Student Object");
+    }
+
+    public void sayHello(){
+        System.out.println("Hello this is student....");
+    }
+}
